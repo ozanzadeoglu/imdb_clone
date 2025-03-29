15dc69e5a140d3c9aab2505d1c411c8f8d86dc75
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-03-29 14:19:33 +0300

    Break apart PeopleDetailsView according to new folder structure. *Convert helper methods with widget return type to individual StatelessWidgets.

---
+++ lib/views/people/widgets/_expandable_biography.dart
@@ -0,0 +1,159 @@
+part of '../people_details_view.dart';
+
+class _ExpandableBiography extends StatelessWidget {
+  const _ExpandableBiography({
+    required this.biography,
+  });
+
+  final String? biography;
+
+  bool isBiographyEmpty() {
+    if (biography == "" || biography == null) {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double containerHeight =
+        Theme.of(context).textTheme.titleLarge!.fontSize! + Paddings.low.value;
+
+    return isBiographyEmpty()
+        ? const SizedBox.shrink()
+        : ExpandableNotifier(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: containerHeight,
+                  decoration: BoxDecoration(
+                    color: ColorConstants.offBlack,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(context.sized.height * 0.02),
+                    ),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    clipBehavior: Clip.none,
+                    children: [
+                      Positioned(
+                        bottom: containerHeight / 2,
+                        child: _BiographyTitleContainer(
+                            containerHeight: containerHeight),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: ColorConstants.offBlack,
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(context.sized.height * 0.02),
+                    ),
+                  ),
+                  child: Expandable(
+                    collapsed: _CollapsedBiography(biography: biography!),
+                    expanded: _ExpandedBiography(biography: biography!),
+                  ),
+                ),
+              ],
+            ),
+          );
+  }
+}
+
+class _BiographyTitleContainer extends StatelessWidget {
+  final double containerHeight;
+
+  const _BiographyTitleContainer({required this.containerHeight});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: containerHeight,
+      decoration: const ShapeDecoration(
+          color: ColorConstants.iconYellow, shape: StadiumBorder()),
+      child: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: context.sized.width * 0.05,
+          ),
+          child: Text(StringConstants.peopleDetailsBiography,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(color: Colors.black, fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
+
+class _CollapsedBiography extends StatelessWidget {
+  final String biography;
+  const _CollapsedBiography({required this.biography});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpandableButton(
+      theme: ExpandableThemeData(
+        inkWellBorderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(context.sized.height * 0.02),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+            Paddings.low.value, 0, Paddings.low.value, Paddings.low.value),
+        child: Column(
+          children: [
+            Text(biography, maxLines: 3, overflow: TextOverflow.ellipsis),
+            const Align(
+              alignment: Alignment.center,
+              child: Icon(Icons.arrow_downward_sharp,
+                  color: ColorConstants.iconYellow),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ExpandedBiography extends StatelessWidget {
+  final String biography;
+  const _ExpandedBiography({required this.biography});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpandableButton(
+      theme: ExpandableThemeData(
+        inkWellBorderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(context.sized.height * 0.02),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+            Paddings.low.value, 0, Paddings.low.value, Paddings.low.value),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              biography,
+            ),
+            const Align(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.arrow_upward_sharp,
+                color: ColorConstants.iconYellow,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/people/widgets/_image_and_info.dart
@@ -0,0 +1,99 @@
+part of '../people_details_view.dart';
+
+class _ImageAndInfoRow extends StatelessWidget {
+  const _ImageAndInfoRow({
+    required this.people,
+  });
+
+  final People people;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomPosterNetworkImage(
+          path: people.imagePath,
+          height: ImageSizes.detailsHeight.value,
+          width: ImageSizes.detailsWidth.value,
+        ),
+        SizedBox(width: context.sized.lowValue),
+        Expanded(
+          child: Column(
+            spacing: Paddings.lowlow.value,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                people.name ?? StringConstants.unknownName,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              _SingleInfoRow(
+                  field: StringConstants.peopleDetailsKnownFor,
+                  value: people.knownForDepartment,
+                  ),
+              _SingleInfoRow(
+                  field: StringConstants.peopleDetailsGender,
+                  value: people.gender,
+                  ),
+              _SingleInfoRow(
+                  field: StringConstants.peopleDetailsBirthday,
+                  value: people.birthday,
+                  ),
+              _SingleInfoRow(
+                  field: StringConstants.peopleDetailsDeathday,
+                  value: people.deathDay,
+                  ),
+              _SingleInfoRow(
+                  field: StringConstants.peopleDetailsPlaceOfBirth,
+                  value: people.placeOfBirth,
+                  ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SingleInfoRow extends StatelessWidget {
+  final String field;
+  final String? value;
+
+  const _SingleInfoRow({
+    required this.field,
+    required this.value,
+  });
+
+  bool isNullOrEmpty(String? string) {
+    if (string == "" || string == null) {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isNullOrEmpty(value)
+        ? const SizedBox.shrink()
+        : Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 4,
+                child: Text("$field: ",
+                    style: Theme.of(context).textTheme.bodyLarge),
+              ),
+              Expanded(
+                flex: 6,
+                child: Text(
+                  value!,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          );
+  }
+}
