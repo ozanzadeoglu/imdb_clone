71baa1d9e46c8dd200d98de5041c5d533fc95a83
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-05-23 17:54:44 +0300

    Create a local db service for Bookmark feature

--- lib/constants/box_names.dart
+++ lib/constants/box_names.dart
@@ -1,3 +1,4 @@
 class BoxNames {
   static const String resentSearchBox = "resentsearchbox";
+  static const String bookmarkBox = "bookmarkBox";
 }
---
+++ lib/services/bookmark/bookmark_service.dart
@@ -0,0 +1,21 @@
+import 'package:imdb_app/constants/box_names.dart';
+import 'package:imdb_app/local_database_managers/hive_manager.dart';
+import 'package:imdb_app/models/bookmark/bookmark_entity.dart';
+
+class BookmarkService extends IHiveManager<BookmarkEntity> {
+  BookmarkService() : super(BoxNames.bookmarkBox);
+
+  @override
+  List<BookmarkEntity>? fetchValues() {
+    return box?.values.toList();
+  }
+
+  @override
+  Future<void> putItem(String key, BookmarkEntity item) async {
+    await box?.put(key, item);
+  }
+
+  Future<void> removeItem(String key, BookmarkEntity item) async {
+    await box?.delete(key);
+  }
+}
