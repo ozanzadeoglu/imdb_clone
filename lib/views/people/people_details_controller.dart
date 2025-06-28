bd0a81d6489f94551f7ac8ace86176a2c4dd4cfe
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-06-28 21:09:07 +0300

    Implement boomark features to be used in ´PeopleDetailsView´

--- lib/views/people/people_details_controller.dart
+++ lib/views/people/people_details_controller.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:imdb_app/enums/media_types.dart';
+import 'package:imdb_app/models/bookmark/bookmarked_people.dart';
 import 'package:imdb_app/models/people.dart';
 import 'package:imdb_app/models/poster_card_media.dart';
+import 'package:imdb_app/services/bookmark/bookmark_service.dart';
 import 'package:imdb_app/services/people_service.dart';
 
 class PeopleDetailsController extends ChangeNotifier {
   final IPeopleService _peopleService = PeopleService();
+  final BookmarkService _bookmarkService = BookmarkService();
+
 
   final int peopleID;
   final String peopleName;
@@ -13,14 +18,46 @@ class PeopleDetailsController extends ChangeNotifier {
   late final Future<List<PosterCardMedia>?> _knownForList;
 
   bool _isLoading = true;
+  bool _isBookmarked = false;
+
 
   People? get people => _people;
   Future<List<PosterCardMedia>?> get knownForList => _knownForList;
 
   bool get isLoading => _isLoading;
+  bool get isBookmarked => _isBookmarked;
+
 
   PeopleDetailsController({required this.peopleID, required this.peopleName}) {
+    _isBookmarked = _checkIfBookmarked();
     _fetchPageData();
+  }
+
+  void addOrRemoveBookmark(){
+    if(!_isBookmarked){
+      _addBookmark();
+    }else{
+      _removeBookmark();
+    }
+    _isBookmarked = _checkIfBookmarked();
+    notifyListeners();
+  }
+
+  //functions below are file private, only functions and variables above will be used in view.
+  
+  void _addBookmark() {
+    if (_people != null) {
+      final bookmarkedPeople = BookmarkedPeople(bookmarkedDate: DateTime.now(), person: people!);
+      _bookmarkService.putItem(bookmarkedPeople.id, bookmarkedPeople);
+    }
+  }
+
+  void _removeBookmark() {
+    _bookmarkService.removeItem("${MediaTypes.person.value}_$peopleID");
+  }
+
+  bool _checkIfBookmarked() {
+    return _bookmarkService.box.keys.contains("${MediaTypes.person.value}_$peopleID");
   }
 
   void _fetchPageData() async {
