48d919deb101f6bb7d2923bb0a576de4cf253e53
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-06-28 20:37:11 +0300

    Remove interface due to not fitting in current architecture, it causes errors on reaching the box in superclass IHiveManager

--- lib/services/bookmark/bookmark_service.dart
+++ lib/services/bookmark/bookmark_service.dart
@@ -1,16 +1,9 @@
-import 'package:hive/hive.dart';
 import 'package:imdb_app/constants/box_names.dart';
 import 'package:imdb_app/local_database_managers/hive_manager.dart';
 import 'package:imdb_app/models/bookmark/bookmark_entity.dart';
 
 
-abstract class IBookmarkService {
-  List<BookmarkEntity>? fetchValues();
-  Future<void> putItem(String key, BookmarkEntity item);
-  Future<void> removeItem(String key);
-}
-
-class BookmarkService extends IHiveManager<BookmarkEntity> implements IBookmarkService{
+class BookmarkService extends IHiveManager<BookmarkEntity>{
   BookmarkService() : super(BoxNames.bookmarkBox);
 
   @override
@@ -22,8 +15,7 @@ class BookmarkService extends IHiveManager<BookmarkEntity> implements IBookmarkS
   Future<void> putItem(String key, BookmarkEntity item) async {
     await box.put(key, item);
   }
-  
-  @override
+
   Future<void> removeItem(String key) async {
     await box.delete(key);
   }
