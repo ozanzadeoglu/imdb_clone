6eca730ee26257e578dd74c694f192bcdad2cc50
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-06-29 21:02:58 +0300

    Implement bookmark remove feature by calling a ConfirmDialog.

--- lib/views/bookmark/bookmark_view.dart
+++ lib/views/bookmark/bookmark_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:imdb_app/components/common/confirm_dialog.dart';
 import 'package:imdb_app/constants/string_constants.dart';
 import 'package:imdb_app/components/common/custom_poster_network_image.dart';
 import 'package:imdb_app/constants/color_constants.dart';
@@ -14,10 +14,7 @@ import 'package:imdb_app/models/bookmark/bookmarked_tv_series.dart';
 import 'package:imdb_app/models/movie.dart';
 import 'package:imdb_app/models/people.dart';
 import 'package:imdb_app/models/tv_series.dart';
-import 'package:imdb_app/utility/navigation_utils.dart';
 import 'package:imdb_app/views/bookmark/bookmark_view_controller.dart';
-import 'package:kartal/kartal.dart';
-import 'package:provider/provider.dart';
 
 part "widgets/_bookmark_poster.dart";
 
@@ -30,6 +27,20 @@ class BookmarkView extends StatefulWidget {
 
 class _BookmarkViewState extends State<BookmarkView> {
   final vm = BookmarkViewController();
+
+  void showRemoveBookmarkDialog(BookmarkEntity item) async {
+    final shouldRemove = await showDialog(
+      context: context,
+      builder: (context) => const ConfirmDialog(
+        title: null,
+        message: StringConstants.bookmarkConfirmDialogMessage,
+        confirmColor: ColorConstants.vermillonCinnabar,
+      ),
+    );
+    if (shouldRemove == true) {
+      vm.removeBookmark(item);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,20 +56,25 @@ class _BookmarkViewState extends State<BookmarkView> {
         body: ValueListenableBuilder<Box<BookmarkEntity>>(
             valueListenable: vm.bookmarkBox.listenable(),
             builder: (context, box, child) {
-             var bookmarks = vm.fetchBookmarks();
+              var bookmarks = vm.fetchBookmarks();
               return ListView.builder(
-                itemCount: bookmarks?.length,
+                itemCount: bookmarks?.length ?? 0,
                 itemBuilder: (context, index) {
+                  final item = bookmarks![index];
                   return Center(
                     child: SizedBox(
-                      height: 120,
+                      height: 160,
                       child: _BookmarkCard.fromType(
-                          item: bookmarks?[index], onTap: null),
+                        item: item,
+                        onBookmarkIconTap: () => showRemoveBookmarkDialog(item),
+                        onCardTap: () {
+                          print("Card Tap");
+                        },
+                      ),
                     ),
                   );
                 },
               );
-            })
-        );
+            }));
   }
 }
